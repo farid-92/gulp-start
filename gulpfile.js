2681d1c275ba9be292ad616ac957f5010ca10e18
Farid <farid.babazov@gmail.com>
2020-02-29 14:23:02 +0600

    fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 /// 			@require   				///////
 ///////////////////////////////////////////////
 var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
 	rename = require('gulp-rename'),
 	sass = require('gulp-sass'),
 	uglify = require('gulp-uglify'),
@@ -9,29 +10,44 @@ var gulp = require('gulp'),
 	rigger = require('gulp-rigger'),
 	autoprefixer = require('gulp-autoprefixer');
 
+var paths = {
+	styles: {
+		src: 'app/sass/**/*.sass',
+		dest: 'app/css/'
+	},
+	templates: {
+		src: 'app/templates/*.html',
+	},
+	html: {
+		src: 'app/build/*.html',
+		dest: 'app/'
+	}
+};
 
 /////////////////////////////////////////////////
 //////  		Tasks					/////////
 /////////////////////////////////////////////////
-gulp.task('sass', function(){
-	gulp.src('app/sass/style.sass')
-    .pipe(sass({outputStyle: 'compact'}).on('error', sass.logError))
+function styles(){
+	return gulp.src(paths.styles.src)
+	.pipe(plumber())
+	.pipe(sass())
     .pipe(autoprefixer({browsers: ['last 10 version'], cascade: false}))
-    .pipe(gulp.dest('app/css/'));
-});
+    .pipe(gulp.dest(paths.styles.dest));
+};
 
-gulp.task('scripts', function(){
+function scripts(){
     gulp.src(['app/js/**/*.js','!app/js/main.min.js'])
     .pipe(uglify())
     .pipe(concat('main.min.js', {newLine: ';'}))
     .pipe(gulp.dest('app/js/'));
-});
+};
 
-gulp.task('html', function(){
-	gulp.src('app/build/*.html')
-	.pipe(rigger())
-	.pipe(gulp.dest('app/'));
-});
+function html(){
+	return gulp.src(paths.html.src)
+		.pipe(plumber())
+		.pipe(rigger())
+		.pipe(gulp.dest(paths.html.dest));
+};
 
 
 
@@ -39,15 +55,15 @@ gulp.task('html', function(){
 /////               Watch              /////////
 ////////////////////////////////////////////////
 
-gulp.task('watch', function () {
-   gulp.watch('app/sass/**/*.sass', gulp.series['sass']);
+function watch() {
+   gulp.watch('app/sass/**/*.sass', styles);
    // gulp.watch('app/js/**/*.js', gulp.series['scripts']);
-   gulp.watch('app/build/*.html', gulp.series['html']);
-   gulp.watch('app/templates/*.html', gulp.series['html']);
-});
+   gulp.watch('app/build/*.html', html);
+   gulp.watch('app/templates/*.html', html);
+};
 
 ////////////////////////////////////////////////
 ////////         Default              //////////
 ////////////////////////////////////////////////
 
-gulp.task('default', gulp.parallel('watch', 'sass', 'html')); // 'scripts'
+gulp.task('default', gulp.parallel(watch)); // 'scripts'
